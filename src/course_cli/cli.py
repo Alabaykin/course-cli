0a03c5016fc2690f70ef447404c8d329f05b8c4f
Alabaykin <115785938+Alabaykin@users.noreply.github.com>
2026-06-25 16:43:49 +0800

    Revert "Feature/init command"

--- src/course_cli/cli.py
+++ src/course_cli/cli.py
@@ -1,8 +1,6 @@
 import sys
 import click
-
 from course_cli.validate import validate_course_structure
-
 
 @click.group()
 def main():
@@ -11,19 +9,10 @@ def main():
 
 @main.command()
 @click.argument('title')
-@click.option('--dir', '-d', 'target_dir', default='.', type=click.Path(), help='Папка для создания курса')
-def init(title, target_dir):
+def init(title):
     """Инициализация структуры нового курса."""
-    click.echo(f"Создание курса: '{title}'...")
-    
-    # Вызов бизнес-логики
-    result = init_course_structure(title, target_dir)
-    
-    # Оформление результата
-    if result.get('is_success'):
-        click.secho(result['message'], fg='green')
-    else:
-        click.secho("❌ Произошла ошибка при создании курса", fg='red')
+    click.echo(f"Создание курса: {title}...")
+    # Здесь будет логика создания папок и файлов
 
 @main.command()
 @click.argument('course_dir', default='.')
